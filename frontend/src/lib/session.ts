852ad62a9f597b05a88734d21b4059a53ca7a69b
SandWithCheese <naufalahmad022@gmail.com>
2025-03-09 14:06:12 +0700

    feat: add db seeding and reset logic

--- frontend/src/lib/session.ts
+++ frontend/src/lib/session.ts
@@ -1,61 +1,61 @@
-// TODO: Fix all error after implementing auth
-import { z } from "zod";
-import { queryClient } from "~/api/client";
-import { User, UserWithRoles } from "~/api/generated";
+// // TODO: Fix all error after implementing auth
+// import { z } from "zod";
+// import { queryClient } from "~/api/client";
+// import { User, UserWithRoles } from "~/api/generated";
 
-export function invalidateSession(user?: User | null) {
-  if (user === undefined) {
-    localStorage.removeItem("user");
-    queryClient.invalidateQueries({ queryKey: ["me"] });
-  } else if (user === null) {
-    localStorage.setItem("user", "null");
-    queryClient.setQueryData(["me"], null);
-  } else {
-    localStorage.setItem("user", JSON.stringify(user));
-    queryClient.setQueryData(["me"], user);
-  }
-}
+// export function invalidateSession(user?: User | null) {
+//   if (user === undefined) {
+//     localStorage.removeItem("user");
+//     queryClient.invalidateQueries({ queryKey: ["me"] });
+//   } else if (user === null) {
+//     localStorage.setItem("user", "null");
+//     queryClient.setQueryData(["me"], null);
+//   } else {
+//     localStorage.setItem("user", JSON.stringify(user));
+//     queryClient.setQueryData(["me"], user);
+//   }
+// }
 
-const userSchema = z
-  .object({
-    id: z.string(),
-    nim: z.string(),
-    email: z.string(),
-    fullName: z.string(),
-    major: z.union([z.literal("IF"), z.literal("STI")]),
-    region: z.union([z.literal("Ganesha"), z.literal("Jatinangor")]),
-    angkatan: z.number(),
-    gender: z.union([z.literal("F"), z.literal("M")]),
-    membershipStatus: z.string(),
-    picture: z.string(),
-    roles: z.array(z.string()),
-  })
-  .nullable();
+// const userSchema = z
+//   .object({
+//     id: z.string(),
+//     nim: z.string(),
+//     email: z.string(),
+//     fullName: z.string(),
+//     major: z.union([z.literal("IF"), z.literal("STI")]),
+//     region: z.union([z.literal("Ganesha"), z.literal("Jatinangor")]),
+//     angkatan: z.number(),
+//     gender: z.union([z.literal("F"), z.literal("M")]),
+//     membershipStatus: z.string(),
+//     picture: z.string(),
+//     roles: z.array(z.string()),
+//   })
+//   .nullable();
 
-export function loadUserCache() {
-  const queryClientUser = queryClient.getQueryData<UserWithRoles | null>([
-    "me",
-  ]);
-  if (queryClientUser !== undefined) {
-    return queryClientUser;
-  }
-  try {
-    const user = localStorage.getItem("user");
-    if (!user) return undefined;
-    return userSchema.parse(JSON.parse(user)) as UserWithRoles;
-  } catch (error) {
-    return undefined;
-  }
-}
+// export function loadUserCache() {
+//   const queryClientUser = queryClient.getQueryData<UserWithRoles | null>([
+//     "me",
+//   ]);
+//   if (queryClientUser !== undefined) {
+//     return queryClientUser;
+//   }
+//   try {
+//     const user = localStorage.getItem("user");
+//     if (!user) return undefined;
+//     return userSchema.parse(JSON.parse(user)) as UserWithRoles;
+//   } catch (error) {
+//     return undefined;
+//   }
+// }
 
-export function tryToLoadUserLocalStorage() {
-  const user = localStorage.getItem("user");
-  if (!user) {
-    throw new Error("User not found");
-  }
-  return userSchema.parse(JSON.parse(user)) as UserWithRoles;
-}
+// export function tryToLoadUserLocalStorage() {
+//   const user = localStorage.getItem("user");
+//   if (!user) {
+//     throw new Error("User not found");
+//   }
+//   return userSchema.parse(JSON.parse(user)) as UserWithRoles;
+// }
 
-export function saveUserCache(user: User | null) {
-  localStorage.setItem("user", JSON.stringify(user));
-}
+// export function saveUserCache(user: User | null) {
+//   localStorage.setItem("user", JSON.stringify(user));
+// }
