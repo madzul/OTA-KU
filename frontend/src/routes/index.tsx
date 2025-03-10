bb717ccbf6dfaa792748fe18ce6953195889f098
randyver <randyverdian99@gmail.com>
2025-03-10 12:28:18 +0700

    feat: button

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,6 +1,7 @@
 import { api } from "@/api/client";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
+import { Button } from "@/components/ui/button";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -17,6 +18,7 @@ function Index() {
   return (
     <div className="p-2 text-4xl text-succeed">
       <h3>Welcome Home!</h3>
+      <Button variant={"default"} size={"lg"}>Click me!</Button>
     </div>
   );
 }
