ef50211021c1aef4d61829416aae427cb8f42333
randyver <randyverdian99@gmail.com>
2025-03-10 14:19:59 +0700

    feat: add toast, input, etc

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,7 +1,7 @@
 import { api } from "@/api/client";
 import { useQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
-import { Button } from "@/components/ui/button";
+import TestComponent from "@/components/test";
 
 export const Route = createFileRoute("/")({
   component: Index,
@@ -16,9 +16,9 @@ function Index() {
   console.log(data);
 
   return (
-    <div className="p-2 text-4xl text-succeed">
+    <div className="p-2 text-4xl">
       <h3>Welcome Home!</h3>
-      <Button variant={"default"} size={"lg"}>Click me!</Button>
+      <TestComponent/>
     </div>
   );
 }
