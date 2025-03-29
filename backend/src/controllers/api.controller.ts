91516063c8a40693e9cebc2b76bfe87c95b48e39
SandWithCheese <naufalahmad022@gmail.com>
2025-03-29 13:19:28 +0700

    feat: add ota and mahasiswa connect post endpoint

--- backend/src/controllers/api.controller.ts
+++ backend/src/controllers/api.controller.ts
@@ -1,6 +1,7 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 
 import { authProtectedRouter, authRouter } from "./auth.controller.js";
+import { connectProtectedRouter, connectRouter } from "./connect.controller.js";
 import { listProtectedRouter, listRouter } from "./list.controller.js";
 import { profileProtectedRouter, profileRouter } from "./profile.controller.js";
 
@@ -8,11 +9,13 @@ const unprotectedApiRouter = new OpenAPIHono();
 unprotectedApiRouter.route("/auth", authRouter);
 unprotectedApiRouter.route("/profile", profileRouter);
 unprotectedApiRouter.route("/list", listRouter);
+unprotectedApiRouter.route("/connect", connectRouter);
 
 const protectedApiRouter = new OpenAPIHono();
 protectedApiRouter.route("/auth", authProtectedRouter);
 protectedApiRouter.route("/profile", profileProtectedRouter);
 protectedApiRouter.route("/list", listProtectedRouter);
+protectedApiRouter.route("/connect", connectProtectedRouter);
 
 export const apiRouter = new OpenAPIHono();
 apiRouter.route("/", unprotectedApiRouter);
