b00101ee7aafac8793426f512dbb9770c0c5da4f
SandWithCheese <naufalahmad022@gmail.com>
2025-03-29 12:44:44 +0700

    feat: add get list mahasiswa ota endpoint

--- backend/src/controllers/api.controller.ts
+++ backend/src/controllers/api.controller.ts
@@ -1,15 +1,18 @@
 import { OpenAPIHono } from "@hono/zod-openapi";
 
 import { authProtectedRouter, authRouter } from "./auth.controller.js";
+import { listProtectedRouter, listRouter } from "./list.controller.js";
 import { profileProtectedRouter, profileRouter } from "./profile.controller.js";
 
 const unprotectedApiRouter = new OpenAPIHono();
 unprotectedApiRouter.route("/auth", authRouter);
 unprotectedApiRouter.route("/profile", profileRouter);
+unprotectedApiRouter.route("/list", listRouter);
 
 const protectedApiRouter = new OpenAPIHono();
 protectedApiRouter.route("/auth", authProtectedRouter);
 protectedApiRouter.route("/profile", profileProtectedRouter);
+protectedApiRouter.route("/list", listProtectedRouter);
 
 export const apiRouter = new OpenAPIHono();
 apiRouter.route("/", unprotectedApiRouter);
