794656deb37352506c7cf100b7ca56fc77437930
SandWithCheese <naufalahmad022@gmail.com>
2025-05-17 15:16:50 +0700

    fix: only send email on production environment

--- backend/src/controllers/otp.controller.ts
+++ backend/src/controllers/otp.controller.ts
@@ -4,11 +4,11 @@ import nodemailer from "nodemailer";
 import { env } from "../config/env.config.js";
 import { db } from "../db/drizzle.js";
 import { accountTable, otpTable } from "../db/schema.js";
+import { otpEmail } from "../lib/email/otp.js";
 import { generateOTP } from "../lib/otp.js";
 import { getOtpExpiredDateRoute, sendOtpRoute } from "../routes/otp.route.js";
 import { SendOtpRequestSchema } from "../zod/otp.js";
 import { createAuthRouter, createRouter } from "./router-factory.js";
-import { otpEmail } from "../lib/email/otp.js";
 
 export const otpRouter = createRouter();
 export const otpProtectedRouter = createAuthRouter();
@@ -46,35 +46,37 @@ otpProtectedRouter.openapi(sendOtpRoute, async (c) => {
 
     //REFERENCE: buat notif
     //createTransport block gada yang diubah
+    if (env.NODE_ENV === "production") {
       const transporter = nodemailer.createTransport({
-      host: "smtp.gmail.com",
-      secure: true,
-      port: 465,
-      auth: {
-        user: env.EMAIL,
-        pass: env.EMAIL_PASSWORD,
-      },
-    });
-
-    transporter.verify((error, success) => {
-      if (error) {
-        console.error("SMTP Server verification failed:", error);
-      } else {
-        console.log("SMTP Server is ready:", success);
-      }
-    });
-
-    //Ubah subject + html
-    await transporter
-      .sendMail({
-        from: env.EMAIL_FROM,
-        to: email,
-        subject: "Token OTP Bantuan Orang Tua Asuh",
-        html: otpEmail(code),
-      })
-      .catch((error) => {
-        console.error("Error sending email:", error);
+        host: "smtp.gmail.com",
+        secure: true,
+        port: 465,
+        auth: {
+          user: env.EMAIL,
+          pass: env.EMAIL_PASSWORD,
+        },
       });
+
+      transporter.verify((error, success) => {
+        if (error) {
+          console.error("SMTP Server verification failed:", error);
+        } else {
+          console.log("SMTP Server is ready:", success);
+        }
+      });
+
+      //Ubah subject + html
+      await transporter
+        .sendMail({
+          from: env.EMAIL_FROM,
+          to: email,
+          subject: "Token OTP Bantuan Orang Tua Asuh",
+          html: otpEmail(code),
+        })
+        .catch((error) => {
+          console.error("Error sending email:", error);
+        });
+    }
 
     return c.json(
       {
