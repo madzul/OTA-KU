ea4dae665ffdf0f0d715d45f81dcbfbbdad7211e
SandWithCheese <naufalahmad022@gmail.com>
2025-05-18 12:42:30 +0700

    feat: add delete account functionality for manajemen akun page

--- frontend/src/routes/_app/manajemen-akun/-components/dropdown-menu-account.tsx
+++ frontend/src/routes/_app/manajemen-akun/-components/dropdown-menu-account.tsx
@@ -8,12 +8,28 @@ import {
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
 import { Eye, MoreHorizontal, Trash2 } from "lucide-react";
+import { useState } from "react";
 
+import DeleteAccountDialog from "./delete-account-dialog";
 import DetailDialog from "./detail-dialog";
 
 function DropdownMenuAccount({ account }: { account: AllAccountListElement }) {
+  const [open, setOpen] = useState(false);
+  const [dialogMenu, setDialogMenu] = useState<string>("none");
+
+  function handleDialogMenu() {
+    switch (dialogMenu) {
+      case "detail":
+        return <DetailDialog account={account} />;
+      case "delete":
+        return <DeleteAccountDialog account={account} setOpen={setOpen} />;
+      default:
+        return null;
+    }
+  }
+
   return (
-    <Dialog>
+    <Dialog open={open} onOpenChange={setOpen}>
       <DropdownMenu>
         <DropdownMenuTrigger asChild>
           <Button variant="ghost" className="h-8 w-8 p-0">
@@ -22,19 +38,23 @@ function DropdownMenuAccount({ account }: { account: AllAccountListElement }) {
           </Button>
         </DropdownMenuTrigger>
         <DropdownMenuContent align="end">
-          <DropdownMenuItem>
-            <DialogTrigger className="flex cursor-pointer gap-2">
+          <DropdownMenuItem onSelect={() => setDialogMenu("detail")} asChild>
+            <DialogTrigger className="flex w-full cursor-pointer gap-2">
               <Eye className="h-4 w-4" />
               Lihat Detail
             </DialogTrigger>
           </DropdownMenuItem>
-          <DropdownMenuItem className="cursor-pointer text-red-600">
-            <Trash2 className="mr-2 h-4 w-4" />
-            Delete
-          </DropdownMenuItem>
+          {account.type !== "admin" && (
+            <DropdownMenuItem onSelect={() => setDialogMenu("delete")} asChild>
+              <DialogTrigger className="flex w-full cursor-pointer gap-2 text-red-600">
+                <Trash2 className="h-4 w-4" />
+                Delete
+              </DialogTrigger>
+            </DropdownMenuItem>
+          )}
         </DropdownMenuContent>
       </DropdownMenu>
-      <DetailDialog account={account} />
+      {handleDialogMenu()}
     </Dialog>
   );
 }
