ecb4e7c427bd5cd10b19e497083c0b86b16e36df
SandWithCheese <naufalahmad022@gmail.com>
2025-05-25 15:34:47 +0700

    feat: add cron notification for reminder update profile and outdated accounts, update setup.ts to includes all the new cron jobs

--- backend/src/cron/setup.ts
+++ backend/src/cron/setup.ts
@@ -1,4 +1,9 @@
-import { dailyReminder7DaysCron, dailyReminderCron } from "./daily-cron.js";
+import {
+  dailyReminder7DaysCron,
+  dailyReminder14DaysCron,
+  dailyReminder30DaysCron,
+  dailyReminderCron,
+} from "./daily-cron.js";
 import { monthlyCron } from "./monthly-cron.js";
 import { everyThreeDaysCron } from "./three-day-cron.js";
 
@@ -6,6 +11,8 @@ export function setupCronJobs() {
   // TODO: Nanti cuma start cron di environment production
   dailyReminderCron.start();
   dailyReminder7DaysCron.start();
+  dailyReminder14DaysCron.start();
+  dailyReminder30DaysCron.start();
   everyThreeDaysCron.start();
   monthlyCron.start();
 }
